8ad71b8bc6d86c409f064d6dbe89647a96b10ac9
Swaraj Shrestha <swarajshrestha@gmail.com>
2023-11-15 13:09:31 -0400

    Socials addon and config files

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -7,36 +7,36 @@ import { BiLogoYoutube } from 'react-icons/bi';
 
 const Socials = () => {
     return (
-            <div>
-                <div className='flex flex-row z-10'>
-                    <div className='flex flex-row'>
-                        <a href="#homepage" className='rounded-full shadow-lg 
+        <div>
+            <div className='flex flex-row z-10'>
+                <div className='flex flex-row'>
+                    <a href="#homepage" className='rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <FaFacebook size={30} />
-                        </a>
+                        <FaFacebook size={30} />
+                    </a>
 
-                        <a href="#gallery" className='rounded-full shadow-lg 
+                    <a href="#gallery" className='rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <FaXTwitter size={30} />
-                        </a>
+                        <FaXTwitter size={30} />
+                    </a>
 
-                        <a href="#burial" className='rounded-full shadow-lg 
+                    <a href="#burial" className='rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <AiOutlineInstagram size={30} />
-                        </a>
+                        <AiOutlineInstagram size={30} />
+                    </a>
 
-                        <a href="#ecosystem" className='rounded-full shadow-lg 
+                    <a href="#ecosystem" className='rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <BiLogoPinterestAlt size={30} />
-                        </a>
+                        <BiLogoPinterestAlt size={30} />
+                    </a>
 
-                        <a href="#about" className='rounded-full shadow-lg 
+                    <a href="#about" className='rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <BiLogoYoutube size={30} />
-                        </a>
-                    </div>
+                        <BiLogoYoutube size={30} />
+                    </a>
                 </div>
             </div>
+        </div>
     );
 };
 export default Socials;
