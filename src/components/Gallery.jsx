01abf004f52d08a43a66794d8df3a9b6df5e5a7f
Istiak15 <kaziistiak821@gmail.com>
2023-11-22 00:57:04 -0400

    project-conservation

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,14 @@
-// Description: This file contains the Gallery component. This component is used to display image and video tiles.
-// Author: Swaraj Shrestha
-
-import Image from "./image.jsx";
-import Video from "./video.jsx";
+//Author: Kazi Istiak
+//This component is designed to display a collection of images and videos in a structured layout.
+import React from 'react';
+import Image from './image.jsx';
+import Video from './video.jsx';
 
 // Gallery component definition
 const Gallery = () => {
     return (
-        // Container for the gallery content
+
+// Container for the entire gallery content
         <div id="gallery" className="border w-full m-auto md:pl-20 p-4 py-16 items-center">
             <h1 className="py-4 text-4xl font-bold text-center text-black">
                 GALLERY
@@ -20,11 +21,21 @@ const Gallery = () => {
                     Fauna
                 </h2>
                 <div className="grid sm:grid-cols-5 gap-5">
-                    <Image imageName="Blue Jay.jpeg" />
-                    <Image imageName="Mallard.jpg" />
-                    <Image imageName="Osprey.jpg" />
-                    <Image imageName="White-tailed Deer.jpg" />
-                    <Image imageName="Common Eastern Bumble Bee.jpg" />
+                    <Image imageName="Merlin.png" description="The Merlin is a small and agile falcon with a rapid, direct flight. It's known for its distinctive facial markings and is often found in open country." />
+                    <Image imageName="Mallard.jpg" description="Mallards are dabbling ducks recognized by the male's glossy green head, white neck ring, and chestnut-brown chest. Females are mottled brown with an orange bill." />
+                    <Image imageName="Osprey.jpg" description="Ospreys are large, fish-eating raptors with a distinctive dark eye stripe. They are often seen near bodies of water, using their sharp talons to catch fish." />
+                    <Image imageName="White-tailed Deer.jpg" description="White-tailed Deer are easily identified by their long, fluffy tails that are brown on top and white underneath. Bucks have antlers, and their coats vary in color." />
+                    <Image imageName="Common Eastern Bumble Bee.jpg" description="These bees are known for their fuzzy appearance and are important pollinators. They have a distinctive black and yellow striped pattern." />
+                    <Image imageName="American Black Bear.jpg" description="Black Bears are large mammals with a robust body, a short tail, and rounded ears. Despite their name, they can be brown, cinnamon, or black." />
+                    <Image imageName="American Red Squirrel.jpeg" description=" Red Squirrels are small, territorial rodents with a reddish-brown coat and a bushy tail. They are active and vocal, often chattering loudly." />
+                    <Image imageName="Barred Owl.jpg" description="Barred Owls have a rounded face and dark eyes, with barred patterns on their feathers." />
+                    <Image imageName="Black-capped Chickadee.jpg" description="Chickadees are small birds with a black cap and bib, white cheeks, and a gray back. They are known for their cheerful 'chick-a-dee-dee-dee' call." />
+                    <Image imageName="Common Garter Snake.jpg" description="Garter Snakes are slender, with longitudinal stripes along their bodies. They are non-venomous and are commonly found in a variety of habitats." />
+                    <Image imageName="Green Frog.jpg" description="Green Frogs are medium-sized frogs with bright green skin. They have prominent tympanic membranes behind their eyes and are often found near water." />
+                    <Image imageName="Japanese Beetle.jpg" description="Japanese Beetles are metallic green and bronze beetles known for their distinctive feeding habits, damaging plants by skeletonizing leaves." />
+                    <Image imageName="Painted Turtle.jpg" description="Painted Turtles have colorful markings on their shells and skin. They are commonly found basking on logs in ponds and are known for their striking appearance." />
+                    <Image imageName="Song Sparrow.jpg" description="Song Sparrows have brown and streaked plumage with a central breast spot. They are known for their varied and melodious songs." />
+                    <Image imageName="Bronze Ground Beetle.jpg" description="Bronze Ground Beetles are predatory beetles with a metallic bronze appearance. They are beneficial in controlling insect pests." />
                 </div>
             </div>
 
@@ -33,10 +44,18 @@ const Gallery = () => {
                     Flora
                 </h2>
                 <div className="grid sm:grid-cols-4 gap-8">
-                    <Image imageName="White Spruce.jpeg" />
-                    <Image imageName="Wild Carrot.jpeg" />
-                    <Image imageName="Pink Lady's Slipper.jpg" />
-                    <Image imageName="Bluebead Lily.jpg" />
+                    <Image imageName="White Spruce.jpeg" description="Description for White Spruce" />
+                    <Image imageName="Wild Carrot.jpeg" description="Description for Wild Carrot" />
+                    <Image imageName="Pink Lady's Slipper.jpg" description="Description for Pink Lady's Slipper" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
+                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
                 </div>
             </div>
         </div>
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,43 +1,74 @@
-// Description: This file contains the Image component. This component is used to display image tiles.
-// Author: Swaraj Shrestha
+//Author: Kazi Istiak
+//This component is used to represent and display individual image tiles within the gallery. It is designed to render an image along with its associated information, such as the image name and description.
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-// Image component definition
-const Image = ({ imageName }) => {
-    // Convert imageName to a format without the file extension
-    let result = toDot(imageName)
+const Image = ({ imageName, description }) => {
+    // State to track whether the image is clicked for full-screen view
+    const [showFullScreen, setShowFullScreen] = useState(false);
+
+    // Click event handler to toggle full-screen view
+    const handleClick = () => {
+        setShowFullScreen(!showFullScreen);
+    };
+
+    // Click event handler to close full-screen view
+    const handleClose = () => {
+        setShowFullScreen(false);
+    };
+
+    // Function to remove file extension from the image name
+    const toDot = (result) => {
+        let dotPosition = result.lastIndexOf('.');
+        if (dotPosition !== -1) {
+            result = result.slice(0, dotPosition);
+        }
+        return result;
+    };
 
     return (
-        // Container for the image
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl 
-            group hover:bg-gradient-to-r from-gray-200 to-[#618c43]">
+        // Container for the image with hover and click functionality
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#618c43]" onClick={handleClick}>
+            {/* Image element with hover effect */}
+            <img className="object-cover rounded-xl group-hover:opacity-25 h-72" src={`images/flora-fauna/${imageName}`} alt=" " />
 
-            <img className="object-cover rounded-xl group-hover:opacity-25 h-full" src={`images/flora-fauna/${imageName}`} alt=" " />
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"> 
+            {/* Hidden div for displaying name when hovering */}
+            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                {/* Show only the name without the file extension when hovering */}
                 <h3 className="text-2xl font-bold text-white tracking-wider text-center">
-                    {result}
+                    {toDot(imageName)}
                 </h3>
             </div>
+
+            {/* Full-screen view when showFullScreen is true */}
+            {showFullScreen && (
+                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-50" onClick={handleClose}>
+                    <div className="relative w-3/4 h-3/4">
+                        {/* Full-size image in the full-screen view */}
+                        <img src={`images/flora-fauna/${imageName}`} alt=" " className="w-full h-full object-cover rounded-xl" />
+                        {/* Description area underneath the full-size image */}
+                        <div className="absolute bottom-0 left-0 w-full p-4 bg-white text-black">
+                            {/* Show the name without the file extension */}
+                            <h3 className="text-2xl font-bold tracking-wider text-center">
+                                {toDot(imageName)}
+                            </h3>
+                            {/* Full description underneath the big image */}
+                            <p>{description}</p>
+                        </div>
+                        {/* Close button for exiting full-screen view */}
+                        <button className="absolute top-0 right-0 m-4 p-2 bg-white rounded-full shadow-lg text-xl cursor-pointer">X</button>
+                    </div>
+                </div>
+            )}
         </div>
-    );   
+    );
 };
 
-// Function to remove the file extension from a string
-function toDot (result) {
-    // Find the position of the dot in the string
-    let dotPosition = result.indexOf('.');
-    // If a dot is found, remove the file extension
-    if (dotPosition !== -1) {
-        result = result.slice(0, dotPosition)
-    }
-    return (
-        result
-    )
+// PropTypes to ensure correct data types for props
+Image.propTypes = {
+    imageName: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
 }
 
-{/* <div className="px-6 py-4">
-<div className="text-center font-bold text-black-500 text-xl mb-2">
-    <h3>{result}</h3>
-</div>
-</div> */}
-
+// Export the Image component
 export default Image;
