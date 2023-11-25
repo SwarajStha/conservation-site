1c43563858a0b0d4653496f28957af2212efbc88
Istiak15 <kaziistiak821@gmail.com>
2023-11-25 19:22:38 -0400

    Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,65 +1,250 @@
 //Author: Kazi Istiak
 //This component is designed to display a collection of images and videos in a structured layout.
-import React from 'react';
-import Image from './image.jsx';
-import Video from './video.jsx';
+//It showcases images of fauna and flora with some functionality, allowing users to load more images dynamically.
+
+// Import necessary dependencies
+import React, { useState } from "react";
+import Image from "./image.jsx";
+import Video from "./video.jsx";
 
 // Gallery component definition
 const Gallery = () => {
-    return (
+  // Array of image objects for Fauna
+  const faunaImagesArray = [
+    {
+      imageName: "Merlin.png",
+      description:
+        "The Merlin is a small and agile falcon with a rapid, direct flight. It's known for its distinctive facial markings and is often found in open country.",
+    },
+    {
+      imageName: "Mallard.jpg",
+      description:
+        "Mallards are dabbling ducks recognized by the male's glossy green head, white neck ring, and chestnut-brown chest. Females are mottled brown with an orange bill.",
+    },
+    {
+      imageName: "Bronze Ground Beetle.jpg",
+      description:
+        "Bronze Ground Beetles are predatory beetles with a metallic bronze appearance. They are beneficial in controlling insect pests.",
+    },
+    {
+      imageName: "Osprey.jpg",
+      description:
+        "Ospreys are large, fish-eating raptors with a distinctive dark eye stripe. They are often seen near bodies of water, using their sharp talons to catch fish.",
+    },
+    {
+      imageName: "White-tailed Deer.jpg",
+      description:
+        "White-tailed Deer are easily identified by their long, fluffy tails that are brown on top and white underneath. Bucks have antlers, and their coats vary in color",
+    },
+    {
+      imageName: "Common Eastern Bumble Bee.jpg",
+      description:
+        "These bees are known for their fuzzy appearance and are important pollinators. They have a distinctive black and yellow striped pattern.",
+    },
+    {
+      imageName: "American Black Bear.jpg",
+      description:
+        "Black Bears are large mammals with a robust body, a short tail, and rounded ears. Despite their name, they can be brown, cinnamon, or black.",
+    },
+    {
+      imageName: "American Red Squirrel.jpeg",
+      description:
+        " Red Squirrels are small, territorial rodents with a reddish-brown coat and a bushy tail. They are active and vocal, often chattering loudly.",
+    },
+    {
+      imageName: "Barred Owl.jpg",
+      description:
+        "Barred Owls have a rounded face and dark eyes, with barred patterns on their feathers.",
+    },
+    {
+      imageName: "Black-capped Chickadee.jpg",
+      description:
+        "Chickadees are small birds with a black cap and bib, white cheeks, and a gray back. They are known for their cheerful 'chick-a-dee-dee-dee' call.",
+    },
+    {
+      imageName: "Common Garter Snake.jpg",
+      description:
+        "Garter Snakes are slender, with longitudinal stripes along their bodies. They are non-venomous and are commonly found in a variety of habitats.",
+    },
+    {
+      imageName: "Green Frog.jpg",
+      description:
+        "Green Frogs are medium-sized frogs with bright green skin. They have prominent tympanic membranes behind their eyes and are often found near water.",
+    },
+    {
+      imageName: "Japanese Beetle.jpg",
+      description:
+        "Japanese Beetles are metallic green and bronze beetles known for their distinctive feeding habits, damaging plants by skeletonizing leaves.",
+    },
+    {
+      imageName: "Painted Turtle.jpg",
+      description:
+        "Painted Turtles have colorful markings on their shells and skin. They are commonly found basking on logs in ponds and are known for their striking appearance.",
+    },
+    {
+      imageName: "Song Sparrow.jpg",
+      description:
+        "Song Sparrows have brown and streaked plumage with a central breast spot. They are known for their varied and melodious songs.",
+    },
+  ];
 
-// Container for the entire gallery content
-        <div id="gallery" className="border w-full m-auto md:pl-20 p-4 py-16 items-center">
-            <h1 className="py-4 text-4xl font-bold text-center text-black">
-                GALLERY
-            </h1>
-            <div className="items-center">
-                <Video />
-            </div>
-            <div>
-                <h2 className="py-4 text-3xl font-bold text-center text-black">
-                    Fauna
-                </h2>
-                <div className="grid sm:grid-cols-5 gap-5">
-                    <Image imageName="Merlin.png" description="The Merlin is a small and agile falcon with a rapid, direct flight. It's known for its distinctive facial markings and is often found in open country." />
-                    <Image imageName="Mallard.jpg" description="Mallards are dabbling ducks recognized by the male's glossy green head, white neck ring, and chestnut-brown chest. Females are mottled brown with an orange bill." />
-                    <Image imageName="Osprey.jpg" description="Ospreys are large, fish-eating raptors with a distinctive dark eye stripe. They are often seen near bodies of water, using their sharp talons to catch fish." />
-                    <Image imageName="White-tailed Deer.jpg" description="White-tailed Deer are easily identified by their long, fluffy tails that are brown on top and white underneath. Bucks have antlers, and their coats vary in color." />
-                    <Image imageName="Common Eastern Bumble Bee.jpg" description="These bees are known for their fuzzy appearance and are important pollinators. They have a distinctive black and yellow striped pattern." />
-                    <Image imageName="American Black Bear.jpg" description="Black Bears are large mammals with a robust body, a short tail, and rounded ears. Despite their name, they can be brown, cinnamon, or black." />
-                    <Image imageName="American Red Squirrel.jpeg" description=" Red Squirrels are small, territorial rodents with a reddish-brown coat and a bushy tail. They are active and vocal, often chattering loudly." />
-                    <Image imageName="Barred Owl.jpg" description="Barred Owls have a rounded face and dark eyes, with barred patterns on their feathers." />
-                    <Image imageName="Black-capped Chickadee.jpg" description="Chickadees are small birds with a black cap and bib, white cheeks, and a gray back. They are known for their cheerful 'chick-a-dee-dee-dee' call." />
-                    <Image imageName="Common Garter Snake.jpg" description="Garter Snakes are slender, with longitudinal stripes along their bodies. They are non-venomous and are commonly found in a variety of habitats." />
-                    <Image imageName="Green Frog.jpg" description="Green Frogs are medium-sized frogs with bright green skin. They have prominent tympanic membranes behind their eyes and are often found near water." />
-                    <Image imageName="Japanese Beetle.jpg" description="Japanese Beetles are metallic green and bronze beetles known for their distinctive feeding habits, damaging plants by skeletonizing leaves." />
-                    <Image imageName="Painted Turtle.jpg" description="Painted Turtles have colorful markings on their shells and skin. They are commonly found basking on logs in ponds and are known for their striking appearance." />
-                    <Image imageName="Song Sparrow.jpg" description="Song Sparrows have brown and streaked plumage with a central breast spot. They are known for their varied and melodious songs." />
-                    <Image imageName="Bronze Ground Beetle.jpg" description="Bronze Ground Beetles are predatory beetles with a metallic bronze appearance. They are beneficial in controlling insect pests." />
-                </div>
-            </div>
+  // Array of image objects for Flora
+  const floraImagesArray = [
+    {
+      imageName: "White Spruce.jpeg",
+      description:
+        "A majestic White Spruce tree with needle-like leaves, common in northern regions. The image likely captures the tree's tall, straight trunk and branches covered in short, green needles.",
+    },
+    {
+      imageName: "Wild Carrot.jpeg",
+      description:
+        "A close-up of the delicate and lacy flowers of Wild Carrot, also known as Queen Anne's Lace. The image may showcase the intricate, umbrella-shaped clusters of small white flowers.",
+    },
+    {
+      imageName: "lily.jpeg",
+      description:
+        "A graceful and elegant Lily, a flowering plant known for its captivating beauty.",
+    },
+    {
+      imageName: "Bluebead Lily.jpg",
+      description:
+        "A Bluebead Lily, featuring delicate white flowers and blue berries. This woodland plant is part of the lily family and is known for its unique blue bead-like fruits.",
+    },
+    {
+      imageName: "sunflower.jpeg",
+      description:
+        "A vibrant Sunflower with its large, golden-yellow petals surrounding a dark central disk. Sunflowers are known for their cheerful appearance and are symbolic of warmth and positivity.",
+    },
+    {
+      imageName: "maples.jpeg",
+      description:
+        " A group of Maple trees, likely showcasing their distinctive lobed leaves. Maples are known for their brilliant autumn foliage and are often associated with Canada.",
+    },
+    {
+      imageName: "Bougainvillea.jpg",
+      description:
+        "A stunning Bougainvillea in full bloom, displaying its vibrant and colorful bracts. Bougainvillea is a tropical plant known for its eye-catching appearance and is often seen in warm climates.",
+    },
+    {
+      imageName: "Pink Lady's Slipper.jpg",
+      description:
+        "Another image capturing the beauty of the Pink Lady's Slipper orchid, emphasizing its unique slipper-shaped pink or magenta flower.",
+    },
+    {
+      imageName: "prickly pears.jpeg",
+      description:
+        "Prickly Pears, a type of cactus, showcasing their flat, paddle-shaped stems and colorful fruits. The fruits are often red or purple and covered in spines.",
+    },
+    {
+      imageName: "yushania.jpeg",
+      description:
+        "Yushania, a type of bamboo, featuring tall and slender green stems. Bamboo is known for its versatility and is used for various purposes.",
+    },
+    {
+      imageName: "spruce.jpeg",
+      description:
+        "A Spruce tree, likely with its characteristic evergreen needles. Spruce trees are common in northern climates and are valued for their timber.",
+    },
+    {
+      imageName: "tulip.jpeg",
+      description:
+        "A vibrant Tulip in full bloom, showcasing its cup-shaped flower and vivid color. Tulips are popular springtime flowers and come in various hues.",
+    },
+  ];
 
-            <div>
-                <h2 className="py-10 text-3xl font-bold text-center text-black">
-                    Flora
-                </h2>
-                <div className="grid sm:grid-cols-4 gap-8">
-                    <Image imageName="White Spruce.jpeg" description="Description for White Spruce" />
-                    <Image imageName="Wild Carrot.jpeg" description="Description for Wild Carrot" />
-                    <Image imageName="Pink Lady's Slipper.jpg" description="Description for Pink Lady's Slipper" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                    <Image imageName="Bluebead Lily.jpg" description="Description for Bluebead Lily" />
-                </div>
-            </div>
+  // Define the initial number of images to show and the increment value for Fauna
+  const initialFaunaImagesToShow = 10;
+  const incrementFauna = 5;
+
+  // State to keep track of the number of Fauna images to show
+  const [faunaImagesToShow, setFaunaImagesToShow] = useState(
+    initialFaunaImagesToShow
+  );
+
+  // Function to handle "Load More" button click for Fauna
+  const handleLoadMoreFauna = () => {
+    setFaunaImagesToShow(faunaImagesToShow + incrementFauna);
+  };
+
+  // Define the initial number of images to show and the increment value for Flora
+  const initialFloraImagesToShow = 8;
+  const incrementFlora = 4;
+
+  // State to keep track of the number of Flora images to show
+  const [floraImagesToShow, setFloraImagesToShow] = useState(
+    initialFloraImagesToShow
+  );
+
+  // Function to handle "Load More" button click for Flora
+  const handleLoadMoreFlora = () => {
+    setFloraImagesToShow(floraImagesToShow + incrementFlora);
+  };
+
+  return (
+    <div
+      id="gallery"
+      className="border w-full m-auto md:pl-20 p-4 py-16 items-center"
+    >
+      <h1 className="py-4 text-4xl font-bold text-center text-black">
+        GALLERY
+      </h1>
+      <div className="items-center">
+        <Video />
+      </div>
+
+      <div>
+        <h2 className="py-4 text-3xl font-bold text-center text-black">
+          Fauna
+        </h2>
+        <div className="grid sm:grid-cols-5 gap-5">
+          {faunaImagesArray.slice(0, faunaImagesToShow).map((image, index) => (
+            <Image
+              key={index}
+              imageName={image.imageName}
+              description={image.description}
+            />
+          ))}
         </div>
-    );
+
+        {faunaImagesToShow < faunaImagesArray.length && (
+          <div className="flex justify-center mt-4">
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+              onClick={handleLoadMoreFauna}
+            >
+              Load More Fauna Images
+            </button>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h2 className="py-10 text-3xl font-bold text-center text-black">
+          Flora
+        </h2>
+        <div className="grid sm:grid-cols-4 gap-8">
+          {floraImagesArray.slice(0, floraImagesToShow).map((image, index) => (
+            <Image
+              key={index}
+              imageName={image.imageName}
+              description={image.description}
+            />
+          ))}
+        </div>
+
+        {floraImagesToShow < floraImagesArray.length && (
+          <div className="flex justify-center mt-4">
+            <button
+              className="px-4 py-2 bg-green-500 text-white rounded-md"
+              onClick={handleLoadMoreFlora}
+            >
+              Load More Flora Images
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,74 +1,90 @@
 //Author: Kazi Istiak
 //This component is used to represent and display individual image tiles within the gallery. It is designed to render an image along with its associated information, such as the image name and description.
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const Image = ({ imageName, description }) => {
-    // State to track whether the image is clicked for full-screen view
-    const [showFullScreen, setShowFullScreen] = useState(false);
+  // State to track whether the image is clicked for full-screen view
+  const [showFullScreen, setShowFullScreen] = useState(false);
 
-    // Click event handler to toggle full-screen view
-    const handleClick = () => {
-        setShowFullScreen(!showFullScreen);
-    };
+  // Click event handler to toggle full-screen view
+  const handleClick = () => {
+    setShowFullScreen(!showFullScreen);
+  };
 
-    // Click event handler to close full-screen view
-    const handleClose = () => {
-        setShowFullScreen(false);
-    };
+  // Click event handler to close full-screen view
+  const handleClose = () => {
+    setShowFullScreen(false);
+  };
 
-    // Function to remove file extension from the image name
-    const toDot = (result) => {
-        let dotPosition = result.lastIndexOf('.');
-        if (dotPosition !== -1) {
-            result = result.slice(0, dotPosition);
-        }
-        return result;
-    };
+  // Function to remove file extension from the image name
+  const toDot = (result) => {
+    let dotPosition = result.lastIndexOf(".");
+    if (dotPosition !== -1) {
+      result = result.slice(0, dotPosition);
+    }
+    return result;
+  };
 
-    return (
-        // Container for the image with hover and click functionality
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#618c43]" onClick={handleClick}>
-            {/* Image element with hover effect */}
-            <img className="object-cover rounded-xl group-hover:opacity-25 h-72" src={`images/flora-fauna/${imageName}`} alt=" " />
+  return (
+    // Container for the image with hover and click functionality
+    <div
+      className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#618c43]"
+      onClick={handleClick}
+    >
+      {/* Image element with hover effect */}
+      <img
+        className="object-cover rounded-xl group-hover:opacity-25 h-72"
+        src={`images/flora-fauna/${imageName}`}
+        alt=" "
+      />
 
-            {/* Hidden div for displaying name when hovering */}
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                {/* Show only the name without the file extension when hovering */}
-                <h3 className="text-2xl font-bold text-white tracking-wider text-center">
-                    {toDot(imageName)}
-                </h3>
+      {/* Hidden div for displaying name when hovering */}
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        {/* Show only the name without the file extension when hovering */}
+        <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+          {toDot(imageName)}
+        </h3>
+      </div>
+
+      {/* Full-screen view when showFullScreen is true */}
+      {showFullScreen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-50"
+          onClick={handleClose}
+        >
+          <div className="relative w-3/4 h-3/4">
+            {/* Full-size image in the full-screen view */}
+            <img
+              src={`images/flora-fauna/${imageName}`}
+              alt=" "
+              className="w-full h-full object-cover rounded-xl"
+            />
+            {/* Description area underneath the full-size image */}
+            <div className="absolute bottom-0 left-0 w-full p-4 bg-white text-black">
+              {/* Show the name without the file extension */}
+              <h3 className="text-2xl font-bold tracking-wider text-center">
+                {toDot(imageName)}
+              </h3>
+              {/* Full description underneath the big image */}
+              <p>{description}</p>
             </div>
-
-            {/* Full-screen view when showFullScreen is true */}
-            {showFullScreen && (
-                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-50" onClick={handleClose}>
-                    <div className="relative w-3/4 h-3/4">
-                        {/* Full-size image in the full-screen view */}
-                        <img src={`images/flora-fauna/${imageName}`} alt=" " className="w-full h-full object-cover rounded-xl" />
-                        {/* Description area underneath the full-size image */}
-                        <div className="absolute bottom-0 left-0 w-full p-4 bg-white text-black">
-                            {/* Show the name without the file extension */}
-                            <h3 className="text-2xl font-bold tracking-wider text-center">
-                                {toDot(imageName)}
-                            </h3>
-                            {/* Full description underneath the big image */}
-                            <p>{description}</p>
-                        </div>
-                        {/* Close button for exiting full-screen view */}
-                        <button className="absolute top-0 right-0 m-4 p-2 bg-white rounded-full shadow-lg text-xl cursor-pointer">X</button>
-                    </div>
-                </div>
-            )}
+            {/* Close button for exiting full-screen view */}
+            <button className="absolute top-0 right-0 m-4 p-2 bg-white rounded-full shadow-lg text-xl cursor-pointer">
+              X
+            </button>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 // PropTypes to ensure correct data types for props
 Image.propTypes = {
-    imageName: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-}
+  imageName: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 // Export the Image component
 export default Image;
