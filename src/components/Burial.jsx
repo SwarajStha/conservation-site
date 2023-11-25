2f93ae2aa1e95b18d023fd57d1a9a5b26397398f
emil <ebero27@gmail.com>
2023-11-25 15:33:36 -0400

    ecosystem minor changes

--- src/components/Burial.jsx
+++ src/components/Burial.jsx
@@ -1,13 +1,31 @@
+import React from "react";
 
 const Burial = () => {
-    return (
-        <div id="burial" className="max-w-[1080px] m-auto p-4 py-16">
-            <h1 className="py-4 text-4xl font-bold text-center text-black">
-                BURIAL
-            </h1>
-            <p>Page content goes here.</p>
+  return (
+    <div>
+      <div id="burial" className="max-w-[1080px] m-auto p-4 py-16 relative">
+        <div>
+          {/* Image */}
+          <img
+            src="src/assets/burial.jpg"
+            alt="picture of burial"
+            className="w-full h-auto"
+          />
         </div>
-    );
+        {/* Text Overlay */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+          <h2 className="text-4xl font-bold">The Burial You Should Know</h2>
+          {/* <p className="mt-2 text-lg">Additional text if needed</p> */}
+        </div>
+        </div>
+        {/* Page Content */}
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white">
+          <h1 className="py-4 text-4xl font-bold">Burial</h1>
+          <p>Page content goes here </p>
+        </div>
+      
+    </div>
+  );
 };
 
 export default Burial;
--- src/components/Ecosystem.jsx
+++ src/components/Ecosystem.jsx
@@ -4,7 +4,9 @@ const Ecosystem = () => {
             <h1 className="py-4 text-4xl font-bold text-center text-black">
                 ECOSYSTEM
             </h1>
-            <p>Page content goes here.</p>
+            <p className="text-2xl font-normal text-gray-700">
+                            French Village Forest Conservation is home to a diverse ecosystem. With historic and interesting flora and fauna, there is something to see for everyone. Whether you are interested in the historic coastal yellow birch that Nova Scotia is famous for, or our small reptile friends that take refuge under rocks; the diversity never stops astonishing.
+            </p>
         </div>
     );
 };
