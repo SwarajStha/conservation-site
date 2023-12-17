e34d882b791ce2f292b5ff02400eee2eafc2f0b8
Istiak15 <kaziistiak821@gmail.com>
2023-12-16 22:03:41 -0400

    Adding project to GitHub repo

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -2,20 +2,19 @@
 
 // Description: This file contains the Video component. This component is used to display video tiles.
 
-import VideoItem from './VideoItem'
+import VideoItem from "./VideoItem";
 
 // Video component definition
 const Video = () => {
-    return (
-        // Container for the video items
-        <div className='w-full m-auto p-4 py-16'>
-
-            <div className='grid sm:grid-cols-2 gap-6 items-center py-5'>
-                <VideoItem video="images/WalkAround.mp4" title='Louisa walking around the conservation area' />
-                <VideoItem video="images/theArea.mp4" title='Expansive spanning view of the conservation area'/>
-            </div>
-        </div>
-    );
+  return (
+    // Container for the video items
+    <div className="w-full m-auto p-4 py-16">
+      <div className="grid sm:grid-cols-2 gap-6 items-center py-5">
+        <VideoItem video="images/Waterfall.mp4" title="Waterfall" />
+        <VideoItem video="images/Forest.mp4" title="Forest" />
+      </div>
+    </div>
+  );
 };
 
 export default Video;
